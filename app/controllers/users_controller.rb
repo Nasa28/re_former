68c4f7aaf93e859d27736c10402467820d2ac842
ixboy <ismaelixboy@gmail.com>
2021-02-26 22:52:49 +0300

    add edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,14 +17,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # def update
-  #   @user
-  # end
-
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to edit_user_path
+      redirect_to new_user_path
     else
       render :edit
     end
